6034f3171c3f8abb17a76a0d291733430e6a0a94
marianordqvist <marianordqvist@live.se>
2023-12-26 15:16:08 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Hello World!</Alert>
+      )}
+      <Button color="success" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
